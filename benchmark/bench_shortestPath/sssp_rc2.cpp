70502a16f32ebf06a529e3c19ddba6edaf6dd64b
Ren Chen <ryanc.usc@gmail.com>
2017-03-13 10:49:31 -0800

    Update sssp_rc2.cpp

--- benchmark/bench_shortestPath/sssp_rc2.cpp
+++ benchmark/bench_shortestPath/sssp_rc2.cpp
@@ -1,4 +1,51 @@
 
+            // check the first arc in A(v) - A_Tk_(v) at Graph g:   neighborNodes_g - neighborNodes_tau_at_g             
+            for (size_t idx=0; idx<neighborNodes_g.size(); ++idx)
+            {
+                //if ( !(find(neighborNodes_tau_at_g.begin(), neighborNodes_tau_at_g.end(), neighborNodes_g[idx].first) != neighborNodes_tau_at_g.end()) ) // try unordered_set
+                if (  neighborNodes_tau_at_g.find(neighborNodes_g[idx].first) == neighborNodes_tau_at_g.end()  ) // --> similar or even slow in small cases.
+                {
+                    // v and x is the cur_deviation_id_g and neighborNodes_g[idx].first, respectively, in MPS alg.
+                    // now add point x and edge v->x in tau  vertex_iterator    
+                    vertex_iterator_tau dest_vit_tau = tau.add_vertex(); // this is x in tau
+
+
+                    edge_iterator eit_g;
+                    bool find_result = g.find_out_edge_2id(cur_deviation_id_g, neighborNodes_g[idx].first, eit_g); // for eit_g->property().cost and eit_g->property().phy_dist
+                    assert(find_result);
+
+                    dest_vit_tau->property().at_KSPaths = false;//dest_vit_tau->property().KSPaths_record.push_back(1); // this node is at the 1st shortest path.
+                    dest_vit_tau->property().predecessor = pk_top_id; // pk_top_id = pk_vkt_nodes.top();
+                    dest_vit_tau->property().internel_id_of_g = neighborNodes_g[idx].first;   
+                    dest_vit_tau->property().min_cost     = pk_top_vit->property().min_cost + eit_g->property().cost; //   
+                    dest_vit_tau->property().sum_distance = pk_top_vit->property().sum_distance + eit_g->property().phy_dist; //   
+                    dest_vit_tau->property().sum_hops     = pk_top_vit->property().sum_hops + 1;
+					
+					unsigned int alpha_occur = (dest_vit_tau->property().occurrence > 7000) ? int(alpha*10.0) : int(alpha*10.0);
+					dest_vit_tau->property().weight     = dest_vit_tau->property().min_cost + dest_vit_tau->property().sum_hops*alpha + dest_vit_tau->property().occurrence*alpha_occur ;
+
+                    uint64_t tau_tmp_id = dest_vit_tau->id();  //  
+                    edge_iterator_tau eit_tau;
+                    tau.add_edge(pk_top_id,tau_tmp_id,eit_tau); // note: put all info at vertex, see dest_vit_tau  (v,x)
+
+   
+                    dest_vit_tau->property().min_cost += g.find_vertex( dest_vit_tau->property().internel_id_of_g )->property().min_cost;
+                    PQ_KSP_candidates_tau.push( pair_FltInt(dest_vit_tau->property().weight, dest_vit_tau->id()) );    //????
+                    dest_vit_tau->property().min_cost -= g.find_vertex( dest_vit_tau->property().internel_id_of_g )->property().min_cost;
+                    //dest_vit_tau->property().sum_distance -= vit_g->property().sum_distance;
+                    //dest_vit_tau->property().sum_hops     -= vit_g->property().sum_hops;
+
+
+                    break;
+                } 
+            } 
+        }//while (!pk_vkt_nodes.empty())  // for each node at pk_vkt
+        
+        iter++;
+
+    } //while (k<Kvalue && !PQ_KSP_candidates_tau.empty() && iter<max_iter)
+    curr_kValue = k;
+
     cout<<"Output: trueMinCost is "<< trueMinCost <<endl;;
     cout<<"--the running iteration (do PQ pop) is "<<iter<<endl;
     cout<<"--the found final path number is "<<KSPaths_lastID_tau.size()<<endl;
@@ -78,23 +125,39 @@ gBenchPerf_event & perf, int perf_group )
     SIM_BEGIN(true);  
 #endif
 
+    double sum_cost     = 0;
+    size_t number_edges = 0;
+    vertex_iterator vit;
+    for (vit=g.vertices_begin(); vit!=g.vertices_end(); vit++)
+    {
+        for (edge_iterator eit = vit->in_edges_begin(); eit != vit->in_edges_end(); eit++)  // new for in edge
+        {
+            if ( abs(FLT_MAX - eit->property().cost) > FLT_MAX/2 )
+            {
+                sum_cost += eit->property().cost;
+                ++number_edges;
+            }
+        }
+    }
+    unsigned int rate = int(sum_cost/number_edges);
+	
     bool trueMinCost        = true;
     size_t curr_kValue      = 0;
+	unordered_set<string> paths;
 
     // first operate trueMinCost with trueMinCost_Iter iterations
-    top_ksp_subFun(trueMinCost, trueMinCost_Iter, curr_kValue, myfile, g, src, dest, Kvalue, max_phy_dist, max_phy_hops, min_phy_hops, 1);
+    //top_ksp_subFun(trueMinCost, trueMinCost_Iter, curr_kValue, myfile, g, src, dest, Kvalue, max_phy_dist, max_phy_hops, min_phy_hops, 1, paths);
 
     // second, find the remaining (Kvalue - curr_kValue) paths by using reduced hops
-	int alpha = 1, rate = 128;
+	int alpha = 1;
 	while (curr_kValue < Kvalue)
     {
 		reset_graph(g);
 		trueMinCost = false;
-		alpha *= rate;
-		top_ksp_subFun(trueMinCost, trueMinCost_Iter, curr_kValue, myfile, g, src, dest, Kvalue, max_phy_dist, max_phy_hops, min_phy_hops, alpha);
+		alpha *= 8; //int(math.sqrt(rate));
+		top_ksp_subFun(trueMinCost, trueMinCost_Iter, curr_kValue, myfile, g, src, dest, Kvalue, max_phy_dist, max_phy_hops, min_phy_hops, alpha, paths);
 	}
     //myfile.close();
-
 #ifdef SIM
     SIM_END(true);
 #endif
@@ -122,42 +185,44 @@ void arg_init(argument_parser & arg)
 vector<vector<int> > loadAggPairs(void)
 {
     vector<vector<int> > res;
+
+   // new data
+    int raw[21][3] = {{0,  1,  18716},
+                      {0,  2,  30006},
+                      {0 , 3 , 16964},
+                      {0     , 334   , 42659 },
+                      {0     , 394   , 42834 },
+                      {0     , 557   , 42983 },
+
+                      {1     , 2     , 31163 },
+                      {1     , 3     , 25949 },
+                      {1     , 334   , 46181 },
+                      {1     , 394   , 46280 },
+
+                      {2     , 557   , 42043 },
+                      {1     , 557   , 46006 },
+                      {2     , 3     , 21733 },
+                      {2     , 334   , 41844 },
+                      {2     , 394   , 41884 },
+
+                      {3     , 334   , 43992 },
+                      {3     , 394   , 44119 },
+                      {3     , 557   , 44324 },
+                      {334   , 394   , 29481 },
+                      {334   , 557   , 29358 },
+                      {394   , 557   , 28410 }};
     
-	int raw[24][3] = {{0,  1,  13036},
-	 {0	 , 334	 , 38338 },
-     {0,  278,  21768    },
-     {1	 , 278	 , 17754 },
-      {0	 , 1	 , 13100 },
-      {0	 , 2	 , 25074 },
-      {0	 , 3	 , 11884 },
-      {0	 , 394	 , 38630 },
-      {0	 , 557	 , 38870 },
-      {1	 , 2	 , 22088 },
-      {1	 , 3	 , 16625 },
-      {1	 , 334	 , 40614 },
-      {1	 , 394	 , 40895 },
-      {1	 , 557	 , 40912 },
-      {2	 , 3	 , 14612 },
-      {2	 , 334	 , 37503 },
-      {2	 , 394	 , 37617 },
-      {2	 , 557	 , 37859 },
-      {334	 , 394	 , 26073 },
-      {334	 , 557	 , 25824 },
-      {394	 , 557	 , 25969 },
-      {3	 , 334	 , 38859 },
-      {3	 , 394	 , 39086 },
-      {3	 , 557	 , 39350 }};
-	
-	for (int i=0; i<24; i++)
-	{
-		vector<int> pair;
-		for (int j=0; j<3; j++)
-			pair.push_back(raw[i][j]);
-		res.push_back(pair);
-	}
-	
+    for (int i=0; i<21; i++)
+    {
+        vector<int> pair;
+        for (int j=0; j<3; j++)
+            pair.push_back(raw[i][j]);
+        res.push_back(pair);
+    }
     return res;
+
 }
+
 
 //Initialize a graph
 void graphInit(graph_t &graph, string vfile, string efile, size_t src, size_t dest, string separator)
@@ -316,7 +381,7 @@ int main(int argc, char * argv[])
     max_phy_dist = FLT_MAX;
     max_phy_hops = 9;
 	min_phy_hops = 4;
-	size_t true_min_iter =  1e6; //1e5; //5*1e6; // 1e6   (0-334:  1e7->152s)
+	size_t true_min_iter =  1e5; //1e5; //5*1e6; // 1e6   (0-334:  1e7->152s)
 	 
     //int total_vertex = 400;  // 400   4000   10000 
     vfile = path + "/Node.csv";  // vertex400   vertex4000    vertex10000
